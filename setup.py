aadc25b96b2a48d59e085925c4f074c699691640
Shreejaa Talla <shreejaa_talla@epam.com>
2022-02-11 16:50:23 -0500

    demo file updated

--- setup.py
+++ setup.py
@@ -7,15 +7,6 @@ import glob
 readme = "README.md"
 requirement_path = CONSTANTS.Requirements_path
 readme_path = os.path.join(os.path.dirname(__file__), readme)
-data_files_01 = glob.glob('petdata/2015/08/01' + '/*.jpg', recursive=True)
-data_files_02 = glob.glob('petdata/2015/08/02' + '/*.jpg', recursive=True)
-data_files_03 = glob.glob('petdata/2015/08/03' + '/*.jpg', recursive=True)
-data_files_04 = glob.glob('petdata/2015/08/04' + '/*.jpg', recursive=True)
-data_files_05 = glob.glob('petdata/2015/08/05' + '/*.jpg', recursive=True)
-data_files_06 = glob.glob('petdata/2015/08/06' + '/*.jpg', recursive=True)
-data_files_07 = glob.glob('petdata/2015/08/07' + '/*.jpg', recursive=True)
-data_files_08 = glob.glob('petdata/2015/08/08' + '/*.jpg', recursive=True)
-data_files_09 = glob.glob('petdata/2015/08/09' + '/*.jpg', recursive=True)
 with open(readme, "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
@@ -26,7 +17,7 @@ pkgs = find_packages(exclude=['tests.*', 'tests'])
 
 setuptools.setup(
     name="augmentation_engine",
-    version="0.1.0",
+    version="0.1.1",
     author="Shreejaa Talla",
     author_email="shreejaa.talla@gmail.com",
     description="Solar Filaments data augmentation demo package",
@@ -37,8 +28,7 @@ setuptools.setup(
     packages = pkgs,
     package_dir={"filament_augmentation": "filament_augmentation"},
     package_data={
-        '.': ['requirements.txt'],
-        '':['petdata/2015']},
+        '.': ['requirements.txt']},
     install_requires=install_requires,
     py_modules=['CONSTANTS'],
     include_package_data=True,
